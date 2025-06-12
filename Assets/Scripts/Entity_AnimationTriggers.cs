df96c1d5cd8d92a56dee9f0ae01d892c8f90fa59
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-06-12 19:56:55 +0300

    target detection

--- Assets/Scripts/Entity_AnimationTriggers.cs
+++ Assets/Scripts/Entity_AnimationTriggers.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class Entity_AnimationTriggers : MonoBehaviour
 {
     private Entity entity;
+    private Entity_CombatComponent entityCombat;
 
     private void Awake()
     {
         entity = GetComponentInParent<Entity>();
+        entityCombat = GetComponentInParent<Entity_CombatComponent>();
     }
 
     private void CurrentStateTrigger()
     {
         entity.CurrentStateAnimationTrigger();
+    }
+
+    private void AttackTrigger()
+    {
+        entityCombat.PerformAttack();
     }
 }
---
+++ Assets/Scripts/Entity_CombatComponent.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Entity_CombatComponent : MonoBehaviour
+{
+
+    [Header("Target Detection")]
+    [SerializeField] private Transform targetCheck;
+    [SerializeField] private LayerMask whatIsTarget;
+    [SerializeField] private float targetCheckRadius = 1;
+
+    public void PerformAttack()
+    {
+
+        foreach (Collider2D c in detectedColliders)
+        {
+            Debug.Log("Attacking " + c.name);
+        }
+    }
+
+    private Collider2D[] detectedColliders
+    {
+        get
+        {
+            return Physics2D.OverlapCircleAll(targetCheck.position, targetCheckRadius, whatIsTarget);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(targetCheck.position, targetCheckRadius);
+    }
+}
